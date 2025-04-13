228a0a74e28e419bf2b385c51987ddc153ba21c4
bbmcnair1 <bbmcnair1@gmail.com>
2025-04-13 11:58:15 -0400

    cleanup

--- headsup/main.py
+++ headsup/main.py
@@ -11,7 +11,7 @@ import numpy as np
 
 def main():
     # Params
-    pull_fresh_data = False
+    pull_fresh_data = True
     etf_list = UNIVERSE['major_indices'] + UNIVERSE['sector_etfs']
     select_symbols = UNIVERSE['stocks']
 
@@ -50,4 +50,42 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+
+    # Params
+    pull_fresh_data = True
+    etf_list = UNIVERSE['major_indices'] + UNIVERSE['sector_etfs']
+    select_symbols = UNIVERSE['stocks']
+
+    # Fetch data from AV API
+    fetcher = DataFetcher(additional_symbols=select_symbols)
+    overviews, prices, financials, data, etf_dict = fetcher.fetch_all_data(etf_list=etf_list,
+                                                                           read_cache=(not pull_fresh_data))
+
+    # Analyze data
+    analyzer = StockAnalyzer(overviews=overviews, data=data, prices=prices, financials=financials, etf_dict=etf_dict,
+                             selected_symbols=select_symbols, add_top_sector_mcaps=True, screen_count=50)
+    index_characteristics, index_returns, sector_characteristics = analyzer.calculate_index_stats()
+    analyzer.grab_select_characteristics()
+    select_returns = analyzer.grab_select_returns()
+    screener, screener_desc, select_characteristics = analyzer.screen_stocks()
+
+    # Plot data
+    plotter = StockPlotter(screener.symbol.unique().tolist()[0:50],
+                           data, prices, financials, overviews, screener, sector_characteristics,
+                           etf_dict, UNIVERSE)
+    plotter.save_plots()
+
+    # Generate report
+    report_gen = ReportGenerator(ic=index_characteristics,
+                                 ir=index_returns,
+                                 sectors=sector_characteristics,
+                                 sc=select_characteristics,
+                                 sr=select_returns,
+                                 screener=screener,
+                                 screener_desc=screener_desc)
+    report_html = report_gen.generate_html_report()
+
+    # Send email
+    email_sender = EmailSender()
+    email_sender.send_report(report_html)
